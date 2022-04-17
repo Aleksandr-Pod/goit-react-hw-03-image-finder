4c4ec3e4265197eea9080d2e8de02bc5b599ad06
Aleksandr-Pod <stdfire@gmail.com>
2022-04-17 09:47:43 +0300

    Update Loader.js

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,8 +3,7 @@ import { Audio } from 'react-loader-spinner'
 
 export default function Loader() { 
     return (<div className="spinner">
-        <Audio/>
-        {/* Используй любой готовый компонент, например react-loader-spinner */}
+        <Audio color="red"/>
         </div>
     )
 }
